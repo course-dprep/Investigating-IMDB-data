f042289ed00df3673b9fcd974da632b7d473c725
joliedielemans <143801073+joliedielemans@users.noreply.github.com>
2023-10-06 15:22:48 +0200

    jhave to commit again

--- src/analysis/Analysis.R
+++ src/analysis/Analysis.R
@@ -1,37 +1,37 @@
 # import dataset
-movie_director_data2 <- read_csv("data/movie_director_data.csv")
-movie_director_data2 <- na.omit(movie_director_data2)
+movie_director_data <- read_csv("data/movie_director_data.csv")
+movie_director_data <- na.omit(movie_director_data)
 
 # make openninggross a numeric variable and remove dollar signs
-movie_director_data2$openinggross <- as.numeric(gsub("[\\$,]", "", movie_director_data2$openinggross))
+movie_director_data$openinggross <- as.numeric(gsub("[\\$,]", "", movie_director_data$openinggross))
 
-#Make runTimeMinutes a dummy variabele with median split
-# Bereken de mediane waarde van de variabele 'runtimeMinutes'
-median_runtime <- median(movie_director_data2$runtimeMinutes)
+# Make runTimeMinutes a dummy variabele by using a median split
+  # Calculate median of variable 'runtimeMinutes'
+median_runtime <- median(movie_director_data$runtimeMinutes)
 
-movie_director_data2$runtime_dummy <- ifelse(movie_director_data2$runtimeMinutes <= median_runtime, 0, 1)
+movie_director_data$runtime_dummy <- ifelse(movie_director_data$runtimeMinutes <= median_runtime, 0, 1)
 
+# Count how many times a director has directed a movie that is in the dataset
+movie_director_data$director_count <- ave(movie_director_data$director_identifier, movie_director_data$director_identifier, FUN = length)
 
-#Count how many times a director has directed a movie that is in the dataset
-movie_director_data2$director_count <- ave(movie_director_data2$director_identifier, movie_director_data2$director_identifier, FUN = length)
+median_director_count <- median(movie_director_data$director_count)
+movie_director_data$directed_above_median <- ifelse(movie_director_data$director_count <= median_runtime, 0, 1)
 
-median_director_count <- median(movie_director_data2$director_count)
-movie_director_data2$directed_above_median <- ifelse(movie_director_data2$director_count <= median_runtime, 0, 1)
-
-
-str(movie_director_data2)
-# make regression
-openinggross_lm1 <- lm(openinggross ~ theathers + directed_above_median, movie_director_data2)
+# Regression analysis
+openinggross_lm1 <- lm(openinggross ~ theathers + directed_above_median + runtime_dummy, movie_director_data)
 summary(openinggross_lm1)
-
-#T-test + Barplot to make the t-test visual ----
-
-#t-test with openinggross DV and director_above_median IV
-t_test_result <- t.test(openinggross ~ directed_above_median, data = movie_director_data2)
+  # The amount of theaters and runtime have a significant effect on the openinggross 
+# T-test + Barplot to make the t-test visual ----
+  # T-test with openinggross DV and director_above_median IV
+t_test_result <- t.test(openinggross ~ directed_above_median, data = movie_director_data)
 print(t_test_result)
 
+  # T-Test: The t-test indicates a significant difference in means between the two groups (directed_above_median 0 and 1) based on the opening gross.
+  # Regression Analysis: The regression analysis, when considering theaters and runtime_dummy as well, does not find directed_above_median to be a significant predictor of opening gross.
+  # The difference arises due to the additional predictors included in the regression analysis. When you include other predictors (like theathers and runtime_dummy), they might account for some of the variation in opening gross that was attributed to directed_above_median in the t-test. In other words, the effect of directed_above_median on opening gross might be explained by the other predictors in the regression model.
+
 # Bereken de gemiddelde Opening Gross per groep
-means <- tapply(movie_director_data2$openinggross / 1e6, movie_director_data2$directed_above_median, mean)
+means <- tapply(movie_director_data$openinggross / 1e6, movie_director_data$directed_above_median, mean)
 
 # Maak een staafdiagram
 barplot(means, 
@@ -45,3 +45,9 @@ barplot(means,
 # Voeg een legenda toe
 legend("topright", legend = c("Directed Below Median", "Directed Above Median"), fill = c("blue", "red"))
 
+# DEZE REGRESSIE WEGHALEN
+# Second regression with theaters as moderator
+openinggross_lm2 <- lm(openinggross ~ theathers*directed_above_median + runtime_dummy, movie_director_data)
+summary(openinggross_lm2)
+  # All variables are significant, including the interaction term, as p-value is below alpha = 0.05
+  # Significance of the interaction term indicates that the effect of  the effect of theaters on opening gross changes by 13,500 units for movies directed by someone above the median, compared to those below the median, assuming runtime is constant
